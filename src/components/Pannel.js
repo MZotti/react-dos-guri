d231b6a77e59811de080f5e65fd66329ba1c4b59
M Zotti <marcos.zotti@doc9.com.br>
2020-03-13 18:35:58 -0300

    Tomamo um pau

--- src/components/Pannel.js
+++ src/components/Pannel.js
@@ -11,46 +11,65 @@ class Pannel extends React.Component {
         this.state = {
             cards : []
         };
-        this.cards = {
-            awainting : [],
-            development : [],
-            testing : [],
-            validation : [],
-            deployment : [],
-            done : []
-        }
     }
 
     componentDidMount(){
-        axios.get(`https://homologa.doc9.com.br/apiti/chamados-sprint.php`)
+        axios.get(`https://sistema.doc9.com.br/apiti/chamados-sprint.php`)
             .then(res => {
                     const cards = res.data;
                     this.setState({ cards });
-                    cardsBuild(this.state.cards);
+                    console.log(this.state.cards);
+                    //cardsBuild(this, this.state.cards);
                 })
+                
     }
 
     render() {
+        //console.log(this.state.cards);
         return (
             <div className="row">
-                <Column id="0" status="awaiting" statusName="Na fila"/>
-                <Column id="1" status="development" statusName="Em desenvolvimento"/>
-                <Column id="2" status="testing" statusName="Em Teste"/>
-                <Column id="3" status="validation" statusName="Em Homologação"/>
-                <Column id="4" status="deployment" statusName="Aguarda Publicação"/>
-                <Column id="5" status="done" statusName="Pronto"/>
+                <Column id="0" status="awaiting" cards={this.state.cards["NA FILA"]} statusName="Na fila"/>
+                <Column id="1" status="development" cards={this.state.cards["EM DESENVOLVIMENTO"]} statusName="Em desenvolvimento"/>
+                <Column id="2" status="testing" cards={this.state.cards["EM TESTE"]} statusName="Em Teste"/>
+                <Column id="3" status="validation" cards={this.state.cards["EM HOMOLOGAÇÃO"]} statusName="Em Homologação"/>
+                <Column id="4" status="deployment" cards={this.state.cards["AGUARDA PUBLICAÇÃO"]} statusName="Aguarda Publicação"/>
+                <Column id="5" status="done"  cards={this.state.cards["PRONTO"]} statusName="Pronto"/>
             </div>
         )
     }
 }
+/*
+function cardsBuild(obj, tickets){
+    tickets.forEach(tickets => {
+        //console.log(tickets.agrupador);
 
-function cardsBuild(tickets){
-    tickets.forEach(ticket => {
-        console.log(tickets);
-        switch(tickets){
-
+        switch(tickets.agrupador){
+            case "NA FILA":
+                obj.cards.awaiting.push(tickets);
+                break;
+            case "EM DESENVOLVIMENTO":
+                obj.cards.development.push(tickets);
+                break;
+            case "EM TESTE":
+                obj.cards.testing.push(tickets);
+                break;
+            case "EM HOMOLOGAÇÃO":
+                obj.cards.validation.push(tickets);
+                break;
+            case "AGUARDA PUBLICAÇÃO":
+                obj.cards.deployment.push(tickets);
+                break;
+            case "PRONTO":
+                obj.cards.done.push(tickets);
+                break;
+            case "PRONTO":
+                obj.cards.done.push(tickets);
+                break;
+            default:
+                break;
         }
     });
-}
 
+}
+*/
 export default Pannel;

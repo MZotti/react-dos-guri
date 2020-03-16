0b7e0a0139f545c1bdb984b72a5769698ded6dcd
MZotti <marcoshzotti@gmail.com>
2020-03-15 22:56:45 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/navbar/Navbar';
 import Pannel from './components/pannel/Pannel';
+import Sustentacao from './components/pannel/Sustentacao';
 import Websocket from './components/Websocket';
+
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Pannel />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+            <Route path="/" exact={true} component={Pannel} />
+            <Route path="/sprint" exact={true} component={Pannel} />
+            <Route path="/sustentacao" component={Sustentacao} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPause } from '@fortawesome/free-solid-svg-icons'
 
@@ -15,18 +17,22 @@ class Navbar extends React.Component {
             items:[
             { 
                 "name": "Sprint",
+                "link": "/sprint",
                 "actived": true         
             },
             { 
                 "name": "Sustentação", 
+                "link": "/sustentacao",
                 "actived": false       
             },
             { 
                 "name": "BI", 
+                "link": "/bi",
                 "actived": false        
             },
             { 
                 "name": "Infraestrutura", 
+                "link": "/infraestrutura",
                 "actived": false     
             }]
         }
@@ -38,7 +44,7 @@ class Navbar extends React.Component {
                 <ul className="navbar-nav mr-auto">
                     { this.state.items.map(item => 
                         <li className={`nav-item ${ item.actived == true ? `active` : `` } `}>
-                            <a className="nav-link" href="#">{item.name}</a>
+                            <Link className="nav-link" to={item.link}>{item.name}</Link>
                         </li>
                     )}
                     <li className="nav-item no-border">
